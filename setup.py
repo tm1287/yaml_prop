4ea780e2448c344c5f923a6a50018bf1474f5142
bechrist <40005251+bechrist@users.noreply.github.com>
2024-07-23 17:01:38 -0500

    update setup

--- setup.py
+++ setup.py
@@ -19,4 +19,11 @@ setup(
 
     yaml_prop is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (as published by the Free Software Foundation) version 3.0 dated June 2007.
     """,
-    packages=['package'])
+    packages=['yaml_prop'],
+    install_requires=['matplotlib',
+                      'numpy',
+                      'scipy',
+                      'pyyaml',
+                      'numexpr',
+                      'pint',
+                      'mip'])
